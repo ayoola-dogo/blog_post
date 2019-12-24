5b56900182e4990b1dc7d553ce93aaabdbc666b2
Ayoola Dogo <pythonworkforay@gmail.com>
2019-12-24 10:14:52 +0000

    fourth commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='blog_home'),
+    path('', views.PostListView.as_view(), name='blog_home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>/', views.UserListView.as_view(), name='user-posts'),
     path('about/', views.about, name='blog_about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.models import User
 
 
 posts = Post.objects.all()
 
 
 # Create your views here.
-def home(request):
-    # Instead of returning a simple HttpResponse, we are rendering the html template
-    context = {'posts': posts, 'title': 'Spot'}
-    return render(request, 'blog/home.html', context=context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-posted_date']
+    paginate_by = 5
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    context_object_name = 'post'
+    # Since 'APP_DIRS': True in settings, django template-loader which is also use by the class based view will look for
+    # templates in a directory name templates in each installed. In that directory it will look for a file structured
+    # <app-name>/<model>_<viewtype>.html
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class UserListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-posted_date')
 
 
 def about(request):
